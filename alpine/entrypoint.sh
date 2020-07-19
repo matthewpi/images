e02ffaa21d8739dc5ecb9f35d1986dbb0227a0dc
Matthew Penner <me@matthewp.io>
2020-07-19 11:26:09 -0600

    Initial Commit

---
+++ alpine/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/ash
+
+#
+# Copyright (c) 2020 Matthew Penner
+#
+# This repository is licensed under the MIT License.
+# https://github.com/matthewpi/images/blob/master/LICENSE.md
+#
+
+cd /home/container
+
+# Set environment variable that holds the Internal Docker IP
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace start command variables
+MODIFIED_START=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo "[container@pterodactyl ~]$ ${MODIFIED_START}"
+
+# Run the modified start command
+eval ${MODIFIED_START}
