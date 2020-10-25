9087c245bc2886e663ca6c929d6e4f1e0269392c
Matthew Penner <me@matthewp.io>
2020-10-25 11:23:45 -0600

    Attempt to fix timezone problems with images

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -7,14 +7,27 @@
 # https://github.com/matthewpi/images/blob/master/LICENSE.md
 #
 
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set the timezone in the container
+echo $TZ > /etc/timezone
+ln -s ../usr/share/zoneinfo/$TZ /etc/localtime
+
+# Switch to the container's working directory
+USER=container
+HOME=/home/container
+export USER
+export HOME
 cd /home/container
 
 # Set environment variable that holds the Internal Docker IP
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-# Replace start command variables
-MODIFIED_START=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo "[container@pterodactyl ~]$ ${MODIFIED_START}"
+# Replace variables in the startup command
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo "[container@pterodactyl ~]$ ${MODIFIED_STARTUP}"
 
-# Run the modified start command
-eval ${MODIFIED_START}
+# Run the startup command
+su -c "eval ${MODIFIED_STARTUP}" container
