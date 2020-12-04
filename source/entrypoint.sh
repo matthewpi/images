22e836fbbf06e56b0d79593a087c374b36bcf613
Matthew Penner <me@matthewp.io>
2020-12-03 19:05:20 -0700

    Add source container, update GitHub Actions workflow

---
+++ source/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/ash
+
+#
+# Copyright (c) 2020 Matthew Penner
+#
+# This repository is licensed under the MIT License.
+# https://github.com/matthewpi/images/blob/master/LICENSE.md
+#
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Switch to the container's working directory
+cd /home/container
+
+# Set environment variable that holds the Internal Docker IP
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace variables in the startup command
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo "[container@pterodactyl ~]$ ${MODIFIED_STARTUP}"
+
+# Run the startup command
+eval ${MODIFIED_STARTUP}
