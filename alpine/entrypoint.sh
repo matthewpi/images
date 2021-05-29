0248368c01cc7663642b9c51e2d5422e5ebcf662
Dane Everitt <dane@daneeveritt.com>
2021-05-29 10:19:11 -0700

    Improve the startup commands and unbreak their execution

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -33,11 +33,13 @@ export INTERNAL_IP
 # Switch to the container's working directory
 cd /home/container || exit 1
 
-# Replace variables in the startup command
-MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
-printf '\033[1m\033[33mcontainer@pterodactyl~ \033[0m'
-echo "${MODIFIED_STARTUP}"
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
-# Run the startup command
-exec env "${MODIFIED_STARTUP}"
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+exec env "$PARSED"
 
